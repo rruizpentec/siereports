1c72c1724c8a83804c9ee8c6bfcc634974c1ed7f
rruizpentec <rruiz@pentec.es>
2016-06-20 14:28:51 +0200

    Travis-ci test 1

--- version.php
+++ version.php
@@ -25,7 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2016040700;  // YYYYMMDDHH (year, month, day, 24-hr time).
-$plugin->requires = 2015051100; // YYYYMMDDHH (This is the release version for Moodle 2.9).
+$plugin->requires = 2015050500; // YYYYMMDDHH (I tried with 2015110900 for 2.9.3 but doesn't work).
+                                // Default plugins has 2015050500 after installation.
 $plugin->component = 'local_siereports';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->cron = 0;
